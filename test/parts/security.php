53bf16fa5a034b97a1e6731505c7d733fcb58733
Gen Taliar <gen@infira.ee>
2022-03-02 18:46:04 +0200

    tests moved to separate project

--- test/parts/security.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new TAllFields();
-$db->varchar("Ed O'Neil");
-$db->varchar2("SQL_INJECTION ';'alter table xyz';");
-checkQuery($db->getSelectQuery(), "SELECT * FROM `all_fields` WHERE `varchar` = 'Ed O\'Neil' AND `varchar2` = 'SQL_INJECTION \';\'alter table xyz\';'");
--- test/pmg.sh
+++
@@ -1,1 +0,0 @@
-/Users/gentaliaru/ws/git/gitHubInfira/poesis-mg/bin/pmg create ./pmg.yaml
